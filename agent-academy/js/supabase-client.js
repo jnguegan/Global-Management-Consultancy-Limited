c2d537371036db76c784a68ce59628f4dae1f9fc
jnguegan <jnguegan@gmail.com>
2026-03-07 20:37:37 +0000

    Update supabase-client.js

--- agent-academy/js/supabase-client.js
+++ agent-academy/js/supabase-client.js
@@ -1,1 +1,4 @@
+const SUPABASE_URL = "https://ysaxtuvuzqkyfmganven.supabase.co";
+const SUPABASE_KEY = "sb_publishable_esfhuUEIcUJEL6z9zryRAA_FGMmUHlw";
 
+const supabaseClient = supabase.createClient(SUPABASE_URL, SUPABASE_KEY);
