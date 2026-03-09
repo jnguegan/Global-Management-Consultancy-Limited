83143e363f3e2188c229f6fd9b3f56520967f600
jnguegan <jnguegan@gmail.com>
2026-03-09 11:03:34 +0000

    Create regulation-map.js

---
+++ agent-academy/js/regulation-map.js
@@ -0,0 +1,8 @@
+window.REGULATION_DOC_MAP = {
+  FFAR: "ffar-2025.pdf",
+  RSTP: "rstp-2025.pdf",
+  STATUTES: "fifa-statutes-2025.pdf",
+  FDC: "fifa-disciplinary-code-2025.pdf",
+  FCE: "fifa-code-of-ethics-2025.pdf",
+  GUARDIANS: "fifa-guardians-toolkit.pdf"
+};
